c0158e86b77ff9f43be48dee613e2f3d13ab8bb3
junrwrld <batistasousajoao@gmail.com>
2024-01-15 13:18:33 -0300

    Implementando as restricoes de movimento

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -15,12 +15,19 @@ namespace xadrezconsole
 
                 while (!partida.terminada)
                 {
+                    try
+                    {
+
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardanda jogada: " + partida.jogadorAtual);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
                     bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
@@ -30,9 +37,16 @@ namespace xadrezconsole
                     Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
 
-                    partida.executaMovimento(origem, destino);
+                        partida.realizaJogada(origem, destino);
 
+                    }
+                    catch(TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
 
                 }
 
