90a7cda505eb4786b324b156b49599a644d7044e
junrwrld <batistasousajoao@gmail.com>
2024-01-16 14:47:24 -0300

    Jogada especial promocao

--- Xadrez-Console/xadrez/PartidaDeXadrez.cs
+++ Xadrez-Console/xadrez/PartidaDeXadrez.cs
@@ -138,11 +138,28 @@ namespace xadrez
         {
             Peca pecaCapturada = executaMovimento(origem, destino);
 
+
             if (estaEmXeque(jogadorAtual))
             {
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -163,7 +180,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+           
             //#jogadaespecial enpassant
 
             if(p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
